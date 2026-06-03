e32d12c99f16a9ba9e55fb1d6fb445c393b5b436
sugaredcookie <sanidhya.pranshu@gmail.com>
2026-06-03 15:50:56 +0530

    testing a lite version for the config engine

---
+++ engine/process-request.js
@@ -0,0 +1,23 @@
+const sourceType = process.env.SOURCE_TYPE;
+
+switch(sourceType) {
+
+    case "JSON":
+        require("./json-handler");
+        break;
+
+    case "CSV":
+        require("./csv-handler");
+        break;
+
+    case "SQL":
+        require("./sql-handler");
+        break;
+
+    case "YAML":
+        require("./yaml-handler");
+        break;
+
+    default:
+        throw new Error("Unsupported source type");
+}
